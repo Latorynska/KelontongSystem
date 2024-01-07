265692cc73847a39a512d1d7d8ef3f4e4743f67b
Latorynska <paksiringkang225@gmail.com>
2024-01-07 17:59:27 +0700

    update : remove report all branches from manager

--- resources/views/transaction/data.blade.php
+++ resources/views/transaction/data.blade.php
@@ -9,6 +9,7 @@
                     </div>
                     <x-table :data="$branches" :filterFields="'[\'name\', \'manager.name\', \'location\']'">
                         <x-slot name="newData">
+                            @hasrole('owner')
                             <button 
                                 type="button" 
                                 class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-500 text-white hover:bg-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -16,6 +17,7 @@
                             >
                                 Export Data
                             </button>
+                            @endhasrole
                         </x-slot>
                         <!-- Table Header -->
                         <x-slot name="header">
